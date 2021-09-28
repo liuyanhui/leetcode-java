27bec4c790b025c4469eaf97fd89b21171d8feec
liuyanhui414 <liuyanhui414@gmail.com>
2021-09-28 11:53:21 +0800

    add 390

---
+++ src/main/java/leetcode/Elimination_Game_390.java
@@ -0,0 +1,144 @@
+package leetcode;
+
+/**
+ * 390. Elimination Game
+ * Medium
+ * ---------------------------
+ * You have a list arr of all integers in the range [1, n] sorted in a strictly increasing order. Apply the following algorithm on arr:
+ * Starting from left to right, remove the first number and every other number afterward until you reach the end of the list.
+ * Repeat the previous step again, but this time from right to left, remove the rightmost number and every other number from the remaining numbers.
+ * Keep repeating the steps again, alternating left to right and right to left, until a single number remains.
+ *
+ * Given the integer n, return the last number that remains in arr.
+ *
+ * Example 1:
+ * Input: n = 9
+ * Output: 6
+ * Explanation:
+ * arr = [1-, 2, 3-, 4, 5-, 6, 7-, 8, 9-]
+ * arr = [2, 4-, 6, 8-]
+ * arr = [2-, 6]
+ * arr = [6]
+ *
+ * Example 2:
+ * Input: n = 1
+ * Output: 1
+ *
+ * Constraints:
+ * 1 <= n <= 10^9
+ */
+public class Elimination_Game_390 {
+    public static int lastRemaining(int n) {
+        return lastRemaining_3(n);
+    }
+
+    /**
+     * 递归法，没有完全理解
+     * 参考思路：
+     * https://leetcode.com/problems/elimination-game/discuss/87116/Java-Recursion-and-Proof
+     * https://leetcode.com/problems/elimination-game/discuss/87128/C-1-line-solution-with-explanation
+     * @param n
+     * @return
+     */
+    public static int lastRemaining_3(int n) {
+        return leftToRight(n);
+    }
+
+    private static int leftToRight(int n) {
+        if (n <= 2) return n;
+        return 2 * rightToLeft(n / 2);
+    }
+
+    private static int rightToLeft(int n) {
+        if (n <= 2) return 1;
+        if (n % 2 == 1) return 2 * leftToRight(n / 2);
+        return 2 * leftToRight(n / 2) - 1;
+    }
+
+    /**
+     * 参考思路：
+     * https://leetcode.com/problems/elimination-game/discuss/87119/JAVA%3A-Easiest-solution-O(logN)-with-explanation
+     *
+     * 验证通过：
+     * Runtime: 3 ms, faster than 56.79% of Java online submissions for Elimination Game.
+     * Memory Usage: 38 MB, less than 66.20% of Java online submissions for Elimination Game.
+     *
+     */
+    public static int lastRemaining_2(int n) {
+        boolean left = true;
+        int remain = n;
+        int head = 1, step = 1;
+        while (remain > 1) {
+            if (left || remain % 2 == 1) {
+                head += step;
+            }
+            remain /= 2;
+            step *= 2;
+            left = !left;
+        }
+        return head;
+    }
+
+    /**
+     * 思路：朴素思路，每轮去掉一半的数据，共需要logN轮
+     *
+     * 逻辑正确，但是超时了
+     * Time Complexity：O(NlogN)
+     *
+     * @param n
+     * @return
+     */
+    public static int lastRemaining_1(int n) {
+        if (n == 1) return 1;
+        int ret = 0;
+        int[] arr = new int[n / 2];
+        //初始化时，就完成了第一轮的删除任务。去掉所有的奇数
+        for (int i = 0; i < n / 2; i++) {
+            arr[i] = 2 * i + 2;
+        }
+        //从第二轮删除开始，即从右向左开始
+        int direct = -1;//遍历的方向
+        for (int i = 0; i < Math.log(n) / Math.log(2); i++) {
+            boolean skip = false;//是否跳过当前数字
+            if (direct == -1) {
+                int j = n / 2 - 1;
+                while (j >= 0) {
+                    if (arr[j] > 0) {
+                        ret = arr[j];//最后一个被去掉的数字就是所求
+                        if (!skip) arr[j] = 0;
+                        skip = !skip;
+                    }
+                    j--;
+                }
+            } else {
+                int j = 0;
+                while (j < n / 2) {
+                    if (arr[j] > 0) {
+                        ret = arr[j];//最后一个被去掉的数字就是所求
+                        if (!skip) arr[j] = 0;
+                        skip = !skip;
+                    }
+                    j++;
+                }
+            }
+            direct = -direct;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        do_func(9, 6);
+        do_func(1, 1);
+        do_func(2, 2);
+        do_func(15000, 8158);
+        do_func(1000000000, 534765398);
+//        System.out.println(Math.log(8518)/Math.log(2));
+    }
+
+    private static void do_func(int n, int expected) {
+        int ret = lastRemaining(n);
+        System.out.println(ret);
+        System.out.println(ret == expected);
+        System.out.println("--------------");
+    }
+}
