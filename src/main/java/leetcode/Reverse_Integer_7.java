635eab29b2dad216240f89d09be8743ff7f0b041
liuyanhui414 <liuyanhui414@gmail.com>
2021-11-05 10:01:47 +0800

    round2:modigy 7

--- src/main/java/leetcode/Reverse_Integer_7.java
+++ src/main/java/leetcode/Reverse_Integer_7.java
@@ -4,11 +4,12 @@ package leetcode;
  *  这里有金矿
  * https://leetcode.com/problems/reverse-integer/
  * 7. Reverse Integer
- * Easy
+ * Medium
  * ----------------------
- * Given a 32-bit signed integer, reverse digits of an integer.
- * Note:
- * Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+ * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+ *
+ * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+ *
  * Example 1:
  * Input: x = 123
  * Output: 321
@@ -26,12 +27,37 @@ package leetcode;
  * Output: 0
  *
  * Constraints:
- * -231 <= x <= 231 - 1
+ * -2^31 <= x <= 2^31 - 1
  */
 public class Reverse_Integer_7 {
 
     public static int reverse(int x) {
-        return reverse_2(x);
+        return reverse_5(x);
+    }
+
+    /**
+     * Round 2 : 2021.11.5 , 难度从easy变为medium
+     * 验证通过：
+     * Runtime: 1 ms, faster than 100.00% of Java online submissions for Reverse Integer.
+     * Memory Usage: 36 MB, less than 76.62% of Java online submissions for Reverse Integer.
+     *
+     * @param x
+     * @return
+     */
+    public static int reverse_5(int x) {
+        if (x == Integer.MIN_VALUE) return 0;
+        int ret = 0;
+        int sign = x > 0 ? 1 : -1;
+        x = sign * x;
+        while (x > 0) {
+            int t = ret * 10 + x % 10;
+            if (t / 10 != ret) {
+                return 0;
+            }
+            ret = ret * 10 + x % 10;
+            x /= 10;
+        }
+        return ret * sign;
     }
 
     /**
@@ -150,10 +176,10 @@ public class Reverse_Integer_7 {
     }
 
     public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
 //        System.out.println(Integer.MIN_VALUE);
 
-//        do_func(123, 321);
+        do_func(123, 321);
         do_func(-123, -321);
         do_func(120, 21);
         do_func(0, 0);
@@ -165,6 +191,7 @@ public class Reverse_Integer_7 {
         do_func(Integer.MIN_VALUE, 0);
         do_func(-2147483412, -2143847412);
         do_func(-2147483648, 0);
+        do_func(1534236469, 0);
     }
 
     private static void do_func(int x, int expected) {
