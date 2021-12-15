bc370e9323619b38a095db4ac3f43bbb61337d3b
liuyanhui414 <liuyanhui414@gmail.com>
2021-12-15 08:57:31 +0800

    round2 modify 38

--- src/main/java/leetcode/Count_and_Say_38.java
+++ src/main/java/leetcode/Count_and_Say_38.java
@@ -12,7 +12,7 @@ package leetcode;
  * To determine how you "say" a digit string, split it into the minimal number of groups so that each group is a contiguous section all of the same character. Then for each group, say the number of characters, then say the character. To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
  *
  * For example, the saying and conversion for digit string "3322251":
- * Given a positive integer n, return the nth term of the count-and-say sequence.
+ * **去掉了这一行Given a positive integer n, return the nth term of the count-and-say sequence.**
  *
  * Example 1:
  * Input: n = 1
@@ -33,7 +33,57 @@ package leetcode;
  */
 public class Count_and_Say_38 {
     public static String countAndSay(int n) {
-        return countAndSay_1(n);
+        return countAndSay_3(n);
+    }
+
+    /**
+     * 迭代法
+     * 验证通过：
+     * Runtime: 47 ms, faster than 5.04% of Java online submissions for Count and Say.
+     * Memory Usage: 60.2 MB, less than 5.40% of Java online submissions for Count and Say.
+     * 
+     * @param n
+     * @return
+     */
+    public static String countAndSay_3(int n) {
+        String ret = "1";
+        for (int i = 2; i <= n; i++) {
+            String t = ret;
+            ret = "";
+            int last = 0;
+            for (int j = 1; j < t.length(); j++) {
+                if (t.charAt(last) != t.charAt(j)) {
+                    ret += String.valueOf(j - last) + String.valueOf(t.charAt(last));
+                    last =j;
+                }
+            }
+            ret += String.valueOf(t.length() - last) + String.valueOf(t.charAt(last));
+        }
+        return ret;
+    }
+
+    /**
+     * round2
+     * 验证通过：
+     * Runtime: 55 ms, faster than 5.02% of Java online submissions for Count and Say.
+     * Memory Usage: 60.2 MB, less than 5.19% of Java online submissions for Count and Say.
+     *
+     * @param n
+     * @return
+     */
+    public static String countAndSay_2(int n) {
+        String ret = "";
+        if (n == 1) return "1";
+        String t = countAndSay(n - 1);
+        int last = 0;
+        for (int i = 1; i < t.length(); i++) {
+            if (t.charAt(last) != t.charAt(i)) {
+                ret += String.valueOf(i - last) + String.valueOf(t.charAt(last));
+                last = i;
+            }
+        }
+        ret += String.valueOf(t.length() - last) + String.valueOf(t.charAt(last));
+        return ret;
     }
 
     /**
