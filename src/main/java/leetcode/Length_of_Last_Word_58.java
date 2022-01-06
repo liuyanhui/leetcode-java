3b1eb34644debc39bedbd602c50e889fbd5d8ada
liuyanhui414 <liuyanhui414@gmail.com>
2022-01-06 09:39:42 +0800

    round-2 modigy 58

--- src/main/java/leetcode/Length_of_Last_Word_58.java
+++ src/main/java/leetcode/Length_of_Last_Word_58.java
@@ -5,25 +5,63 @@ package leetcode;
  * 58. Length of Last Word
  * Easy
  * ----------------------
- * Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+ * Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
  *
  * A word is a maximal substring consisting of non-space characters only.
  *
  * Example 1:
  * Input: s = "Hello World"
  * Output: 5
+ * Explanation: The last word is "World" with length 5.
  *
  * Example 2:
- * Input: s = " "
- * Output: 0
+ * Input: s = "   fly me   to   the moon  "
+ * Output: 4
+ * Explanation: The last word is "moon" with length 4.
+ *
+ * Example 3:
+ * Input: s = "luffy is still joyboy"
+ * Output: 6
+ * Explanation: The last word is "joyboy" with length 6.
  *
  * Constraints:
- * 1 <= s.length <= 104
+ * 1 <= s.length <= 10^4
  * s consists of only English letters and spaces ' '.
+ * There will be at least one word in s.
  */
 public class Length_of_Last_Word_58 {
     public static int lengthOfLastWord(String s) {
-        return lengthOfLastWord1_3(s);
+        return lengthOfLastWord_4(s);
+    }
+
+    /**
+     * round-2
+     * review 完成耗时有点长
+     *
+     * 从后向前遍历，遍历两次。
+     * 1.第一次遍历，跳过结尾的空格
+     * 2.第二次遍历，如果字符不是空白字符，count++
+     *
+     * 验证通过：
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Length of Last Word.
+     * Memory Usage: 38.8 MB, less than 25.04% of Java online submissions for Length of Last Word.
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLastWord_4(String s) {
+        int count = 0;
+        int i = s.length() - 1;
+        while (i >= 0) {
+            if (s.charAt(i) != ' ') break;
+            else i--;
+        }
+        while (i >= 0) {
+            if (s.charAt(i) == ' ') break;
+            else count++;
+            i--;
+        }
+        return count;
     }
 
     /**
@@ -82,6 +120,8 @@ public class Length_of_Last_Word_58 {
         do_func("Today is a nice day", 3);
         do_func("a", 1);
         do_func("swap", 4);
+        do_func("   fly me   to   the moon  ", 4);
+        do_func("luffy is still joyboy", 6);
     }
 
     private static void do_func(String s, int expected) {
