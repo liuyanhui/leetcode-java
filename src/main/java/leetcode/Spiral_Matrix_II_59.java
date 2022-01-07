e49e159c61986b6ff0394b54856cd16a383b1073
liuyanhui414 <liuyanhui414@gmail.com>
2022-01-07 08:51:27 +0800

    round-2 modify 59

--- src/main/java/leetcode/Spiral_Matrix_II_59.java
+++ src/main/java/leetcode/Spiral_Matrix_II_59.java
@@ -5,7 +5,7 @@ package leetcode;
  * 59. Spiral Matrix II
  * Medium
  * ----------------------
- * Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+ * Given a positive integer n, generate an n x n matrix filled with elements from 1 to n^2 in spiral order.
  *
  * Example 1:
  * Input: n = 3
@@ -21,7 +21,52 @@ package leetcode;
 public class Spiral_Matrix_II_59 {
 
     public static int[][] generateMatrix(int n) {
-        return generateMatrix_1(n);
+        return generateMatrix_3(n);
+    }
+
+    /**
+     * 类似Spiral_Matrix_54的思路，算法如下：
+     * 1.初始化一个n*n的二位数字，num=1
+     * 2.按正方形环计算，每个环都分为四个方向（向右->向下->向左->向上），每放置一个数字之后num++。
+     * 3.共(n+1)/2个环。每环的边长为n-cricle*2，circle表示环编号从0开始计数。
+     * 4.当n-cricle*2<=0时，表示计算结束
+     * 5.当n-cricle*2==1时，为由一个点组成的环。
+     * 
+     * 验证通过：
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Spiral Matrix II.
+     * Memory Usage: 38.8 MB, less than 14.02% of Java online submissions for Spiral Matrix II.
+     *
+     * @param n
+     * @return
+     */
+    public static int[][] generateMatrix_3(int n) {
+        int[][] ret = new int[n][n];
+        int num = 1, circle = 0;
+        while (n - circle * 2 > 0) {
+            if ((n - circle * 2) == 1) {
+                ret[n / 2][n / 2] = num;
+                break;
+            }
+            //每个方向的最后一个元素都由下一个方向处理，作为下一个方向的第一个元素
+            //go right
+            for (int i = circle; i < n - circle - 1; i++) {
+                ret[circle][i] = num++;
+            }
+            //go down
+            for (int i = circle; i < n - circle - 1; i++) {
+                ret[i][n - 1 - circle] = num++;
+            }
+            //go left
+            for (int i = n - 1 - circle; i > circle; i--) {
+                ret[n - 1 - circle][i] = num++;
+            }
+            //go up
+            for (int i = n - 1 - circle; i > circle; i--) {
+                ret[i][circle] = num++;
+            }
+            circle++;
+        }
+        return ret;
     }
 
     /**
@@ -94,20 +139,13 @@ public class Spiral_Matrix_II_59 {
     public static void main(String[] args) {
         do_func(3, new int[][]{{1, 2, 3}, {8, 9, 4}, {7, 6, 5}});
         do_func(1, new int[][]{{1}});
+        do_func(20, new int[][]{{1}});
     }
 
     private static void do_func(int n, int[][] expected) {
         int[][] ret = generateMatrix(n);
-        boolean same = true;
-        for (int i = 0; i < ret.length; i++) {
-            for (int j = 0; j < ret[0].length; j++) {
-                if (ret[i][j] != expected[i][j]) {
-                    same = false;
-                    break;
-                }
-            }
-        }
-        System.out.println(same);
+        ArrayUtils.printIntArray(ret);
+        System.out.println(ArrayUtils.isSame(ret, expected));
         System.out.println("-----------");
     }
 }
