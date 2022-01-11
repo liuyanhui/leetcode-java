0be826ce12258627b8544c1d666a2987018e6069
liuyanhui414 <liuyanhui414@gmail.com>
2022-01-11 13:39:34 +0800

    round2 modify 62

--- src/main/java/leetcode/Unique_Paths_62.java
+++ src/main/java/leetcode/Unique_Paths_62.java
@@ -1,45 +1,86 @@
 package leetcode;
 
-import java.util.List;
-
 /**
  * https://leetcode.com/problems/unique-paths/
  * 62. Unique Paths
  * Medium
  * --------------
- * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
- * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
- * How many possible unique paths are there?
+ * There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+ *
+ * Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+ *
+ * The test cases are generated so that the answer will be less than or equal to 2 * 10^9.
  *
  * Example 1:
  * Input: m = 3, n = 7
  * Output: 28
  *
- *  Example 2:
+ * Example 2:
  * Input: m = 3, n = 2
  * Output: 3
- * Explanation:
- * From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+ * Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
  * 1. Right -> Down -> Down
  * 2. Down -> Down -> Right
  * 3. Down -> Right -> Down
  *
- *  Example 3:
- * Input: m = 7, n = 3
- * Output: 28
- *
- *  Example 4:
- * Input: m = 3, n = 3
- * Output: 6
- *
  * Constraints:
  * 1 <= m, n <= 100
- * It's guaranteed that the answer will be less than or equal to 2 * 109.
  */
 public class Unique_Paths_62 {
 
     public static int uniquePaths(int m, int n) {
-        return uniquePaths_dp(m, n);
+        return uniquePaths_dp3(m, n);
+    }
+
+    /**
+     * round2
+     * uniquePaths_dp2优化版：二维dp数组该位一维dp数组
+     *
+     * 验证通过：
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Unique Paths.
+     * Memory Usage: 37.8 MB, less than 14.89% of Java online submissions for Unique Paths.
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public static int uniquePaths_dp3(int m, int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+
+    /**
+     * round2
+     * dp思路：dp[m][n]。因为只能向右和向下移动，所以dp[i,j]依赖于其左边和上边的路径数量。
+     * 公式为:dp[i][j]=dp[i-1][j]+dp[i][j-1]，return dp[m-1][n-1]
+     *
+     * 验证通过：
+     * Runtime: 1 ms, faster than 19.48% of Java online submissions for Unique Paths.
+     * Memory Usage: 37.4 MB, less than 25.45% of Java online submissions for Unique Paths.
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public static int uniquePaths_dp2(int m, int n) {
+        int[][] dp = new int[m][n];//可以优化成一维数组
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][0] = 1;
+                    dp[0][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 
     /**
