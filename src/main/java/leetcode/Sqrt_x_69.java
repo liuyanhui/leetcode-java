52acd8e8c4980aaea009b6afebc21aae0a662176
liuyanhui414 <liuyanhui414@gmail.com>
2022-01-17 16:31:06 +0800

    round 2 modify 69

--- src/main/java/leetcode/Sqrt_x_69.java
+++ src/main/java/leetcode/Sqrt_x_69.java
@@ -8,6 +8,8 @@ package leetcode;
  * Given a non-negative integer x, compute and return the square root of x.
  * Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
  *
+ * Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+ *
  * Example 1:
  * Input: x = 4
  * Output: 2
@@ -22,6 +24,12 @@ package leetcode;
  */
 public class Sqrt_x_69 {
 
+    /**
+     * round 2
+     * review 20220117
+     * @param x
+     * @return
+     */
     public static int mySqrt(int x) {
         return mySqrt_2(x);
     }
