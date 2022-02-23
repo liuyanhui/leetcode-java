9d43aa298acacbd7fbd640852b95d571e0858d72
liuyanhui414 <liuyanhui414@gmail.com>
2022-02-23 13:46:28 +0800

    round 2 , mofify 93

--- src/main/java/leetcode/Restore_IP_Addresses_93.java
+++ src/main/java/leetcode/Restore_IP_Addresses_93.java
@@ -7,9 +7,10 @@ import java.util.List;
  * 93. Restore IP Addresses
  * Medium
  * ---------------
- * Given a string s containing only digits, return all possible valid IP addresses that can be obtained from s. You can return them in any order.
+ * A valid IP address consists of exactly four integers separated by single dots. Each integer is between 0 and 255 (inclusive) and cannot have leading zeros.
+ * For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses, but "0.011.255.245", "192.168.1.312" and "192.168@1.1" are invalid IP addresses.
  *
- * A valid IP address consists of exactly four integers, each integer is between 0 and 255, separated by single dots and cannot have leading zeros. For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses and "0.011.255.245", "192.168.1.312" and "192.168@1.1" are invalid IP addresses.
+ * Given a string s containing only digits, return all possible valid IP addresses that can be formed by inserting dots into s. You are not allowed to reorder or remove any digits in s. You may return the valid IP addresses in any order.
  *
  * Example 1:
  * Input: s = "25525511135"
@@ -20,24 +21,69 @@ import java.util.List;
  * Output: ["0.0.0.0"]
  *
  * Example 3:
- * Input: s = "1111"
- * Output: ["1.1.1.1"]
- *
- * Example 4:
- * Input: s = "010010"
- * Output: ["0.10.0.10","0.100.1.0"]
- *
- * Example 5:
  * Input: s = "101023"
  * Output: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
  *
  * Constraints:
- * 0 <= s.length <= 3000
+ * 1 <= s.length <= 20
  * s consists of digits only.
  */
 public class Restore_IP_Addresses_93 {
     public static List<String> restoreIpAddresses(String s) {
-        return restoreIpAddresses_3(s);
+        return restoreIpAddresses_4(s);
+    }
+
+    /**
+     * round 2
+     *
+     * 递归思路
+     *
+     * 验证通过：
+     * Runtime: 2 ms, faster than 92.64% of Java online submissions for Restore IP Addresses.
+     * Memory Usage: 43.1 MB, less than 26.14% of Java online submissions for Restore IP Addresses.
+     *
+     * @param s
+     * @return
+     */
+    public static List<String> restoreIpAddresses_4(String s) {
+        List<String> ret = new ArrayList<>();
+        if (s.length() < 4 || s.length() > 12) return ret;
+        List<String> addr = new ArrayList<>();
+        dfs(s, 0, addr, ret);
+        return ret;
+    }
+
+    private static void dfs(String s, int beg, List<String> addr, List<String> ret) {
+        if (addr.size() == 3) {
+            String t = s.substring(beg);
+            if (validate(t)) {
+                StringBuilder ip = new StringBuilder();
+                for (int i = 0; i < addr.size(); i++) {
+                    ip.append(addr.get(i));
+                    ip.append(".");
+                }
+                ip.append(t);
+                ret.add(ip.toString());
+            }
+        } else {
+            for (int i = 1; i < 4 && i + beg <= s.length(); i++) {
+                String t = s.substring(beg, i + beg);
+                if (validate(t)) {
+                    addr.add(t);
+                    dfs(s, i + beg, addr, ret);
+                    addr.remove(addr.size() - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static boolean validate(String s) {
+        if (s == null || s.length() == 0 || s.length() > 3) return false;
+        if (s.length() > 1 && s.charAt(0) == '0') return false;
+        if (Integer.valueOf(s) > 255) return false;
+        return true;
     }
 
     /**
