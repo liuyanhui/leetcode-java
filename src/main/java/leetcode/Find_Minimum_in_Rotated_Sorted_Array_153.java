01973e976bf6666454b502818d20cec4d8033f5b
liuyanhui414 <liuyanhui414@gmail.com>
2022-06-17 09:05:20 +0800

    round 2 ,modify 153

--- src/main/java/leetcode/Find_Minimum_in_Rotated_Sorted_Array_153.java
+++ src/main/java/leetcode/Find_Minimum_in_Rotated_Sorted_Array_153.java
@@ -36,6 +36,48 @@ package leetcode;
  * nums is sorted and rotated between 1 and n times.
  */
 public class Find_Minimum_in_Rotated_Sorted_Array_153 {
+    public static int findMin(int[] nums) {
+        return findMin_2(nums);
+    }
+
+    /**
+     * round 2
+     * 二分查找的两种情况：
+     * 1.数组有序通过下标比较
+     * 2.数组无序通过值比较
+     * 本题属于第1种情况
+     *
+     * FIXME 在未排序的集合中查找某个数，采用基于数字的binary search；在已排序的集合中查找某个数，采用基于下标的binary search
+     *
+     * 算法如下：
+     * 通过二分查找分割后，共有三种情况：
+     * 1.nums[mid]>nums[right] 说明 mid在左半区间，那么 left = mid+1 //最小值一定在右半部分，所以mid+1
+     * 2.nums[left]>nums[mid] 说明 mid在右半区间，那么 right = mid //最小值一定在右半部分，所以mid不能减1
+     * 3.说明 数组是单调递增的，那么返回nums[left]即可
+     *
+     * 验证通过：
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Find Minimum in Rotated Sorted Array.
+     * Memory Usage: 41.9 MB, less than 83.86% of Java online submissions for Find Minimum in Rotated Sorted Array.
+     *
+     * @param nums
+     * @return
+     */
+    public static int findMin_2(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        int mid = 0;
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[left] > nums[mid]) {
+                right = mid;
+            } else {
+                break;
+            }
+        }
+        return nums[left];
+    }
+
     /**
      * 金矿：在未排序的集合中查找某个数，采用基于数字的binary search；在已排序的集合中查找某个数，采用基于下标的binary search
      * 与Find_the_Duplicate_Number_287和Kth_Smallest_Element_in_a_Sorted_Matrix_378可以组成一个系列
@@ -50,7 +92,7 @@ public class Find_Minimum_in_Rotated_Sorted_Array_153 {
      * @param nums
      * @return
      */
-    public static int findMin(int[] nums) {
+    public static int findMin_1(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         int low = 0, high = nums.length - 1;
         while (low < high) {
