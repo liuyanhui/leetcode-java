596d2aa6f1101f7a7d7a956224623e0385018aae
liuyanhui414 <liuyanhui414@gmail.com>
2022-06-27 17:28:27 +0800

    round 2 ,modify 167

--- src/main/java/leetcode/Two_Sum_II_Input_array_is_sorted_167.java
+++ src/main/java/leetcode/Two_Sum_II_Input_array_is_sorted_167.java
@@ -9,35 +9,76 @@ import java.util.Map;
  * 167. Two Sum II - Input array is sorted
  * Easy
  * --------------
- * Given an array of integers numbers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+ * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
  *
- * Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+ * Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
  *
- * You may assume that each input would have exactly one solution and you may not use the same element twice.
+ * The tests are generated such that there is exactly one solution. You may not use the same element twice.
+ *
+ * Your solution must use only constant extra space.
  *
  * Example 1:
  * Input: numbers = [2,7,11,15], target = 9
  * Output: [1,2]
- * Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+ * Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
  *
  * Example 2:
  * Input: numbers = [2,3,4], target = 6
  * Output: [1,3]
+ * Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
  *
  * Example 3:
  * Input: numbers = [-1,0], target = -1
  * Output: [1,2]
+ * Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
  *
  * Constraints:
- * 2 <= numbers.length <= 3 * 104
+ * 2 <= numbers.length <= 3 * 10^4
  * -1000 <= numbers[i] <= 1000
- * numbers is sorted in increasing order.
+ * numbers is sorted in non-decreasing order.
  * -1000 <= target <= 1000
- * Only one valid answer exists.
+ * The tests are generated such that there is exactly one solution.
  */
 public class Two_Sum_II_Input_array_is_sorted_167 {
     public static int[] twoSum(int[] numbers, int target) {
         return twoSum_2(numbers, target);
+    }
+
+    /**
+     * Two Sum中的哈希表方案可以实现，但是空间复杂度不满足要求。
+     *
+     * 采用双指针夹逼法
+     * 算法如下：
+     * 0.l=0,r=numbers.length-1
+     * 1.当l<r时，循环
+     * 1.1 如果[l]+[r]<targeet，那么l++
+     * 1.2 如果[l]+[r]>targeet，那么r--
+     * 1.3 如果[l]+[r]==targeet，返回[l+1,r+1]
+     *
+     * 验证通过：
+     * Runtime: 2 ms, faster than 75.81% of Java online submissions for Two Sum II - Input Array Is Sorted.
+     * Memory Usage: 50.6 MB, less than 10.10% of Java online submissions for Two Sum II - Input Array Is Sorted.
+     *
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public static int[] twoSum_3(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
+        while (l < r) {
+            int s = numbers[l] + numbers[r];
+            if (s > target) {
+                r--;
+            } else if (s < target) {
+                l++;
+            } else {
+                break;
+            }
+        }
+        int[] ret = new int[2];
+        ret[0] = l + 1;
+        ret[1] = r + 1;
+        return ret;
     }
 
     /**
