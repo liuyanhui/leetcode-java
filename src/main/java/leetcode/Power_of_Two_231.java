b14586589b4f11f02c31b8e729ffd7ff1d862125
liuyanhui414 <liuyanhui414@gmail.com>
2022-09-07 16:40:22 +0800

    round 2 ,modify 231

--- src/main/java/leetcode/Power_of_Two_231.java
+++ src/main/java/leetcode/Power_of_Two_231.java
@@ -37,7 +37,51 @@ package leetcode;
  */
 public class Power_of_Two_231 {
     public static boolean isPowerOfTwo(int n) {
-        return isPowerOfTwo_2(n);
+        return isPowerOfTwo_4(n);
+    }
+
+    /**
+     * AC 中更巧妙的方法
+     *
+     * @param n
+     * @return
+     */
+    public boolean isPowerOfTwo_5(int n) {
+        if (n == 0 || n == 3) return false;
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+        return (n == 1);
+    }
+
+    /**
+     *
+     * 验证通过：
+     * Runtime: 2 ms, faster than 71.39% of Java online submissions for Power of Two.
+     * Memory Usage: 41.2 MB, less than 66.41% of Java online submissions for Power of Two.
+     *
+     * @param n
+     * @return
+     */
+    public static boolean isPowerOfTwo_4(int n) {
+        int t = 0;
+        for (int i = 0; i < 32; i++) {
+            t = 1 << i;
+            if (t == n) return true;
+            if (t > n) break;
+        }
+        return false;
+    }
+
+    public static boolean isPowerOfTwo_3(int n) {
+        if (n == 1) return true;
+        int t = 1;
+        for (int i = 1; i < 32; i++) {
+            t = t * 2;
+            if (t == n) return true;
+            if (t > n) break;
+        }
+        return false;
     }
 
     /**
@@ -53,7 +97,7 @@ public class Power_of_Two_231 {
      * @return
      */
     public static boolean isPowerOfTwo_2(int n) {
-        if(n<=0) return false;
+        if (n <= 0) return false;
         return (n & (n - 1)) == 0;
     }
 
@@ -85,6 +129,7 @@ public class Power_of_Two_231 {
         do_func(5, false);
         do_func(-4, false);
         do_func(0, false);
+        do_func(Integer.MAX_VALUE, false);
 
     }
 
