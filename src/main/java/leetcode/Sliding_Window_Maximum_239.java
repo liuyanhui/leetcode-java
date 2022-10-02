dc67b1fcde42b2b7ae13c142a1b2cbc73544dcec
liuyanhui414 <liuyanhui414@gmail.com>
2022-10-02 10:23:14 +0800

    add solution for 239

--- src/main/java/leetcode/Sliding_Window_Maximum_239.java
+++ src/main/java/leetcode/Sliding_Window_Maximum_239.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.TreeMap;
 
 /**
@@ -35,7 +37,89 @@ import java.util.TreeMap;
 public class Sliding_Window_Maximum_239 {
 
     public static int[] maxSlidingWindow(int[] nums, int k) {
-        return maxSlidingWindow_1(nums, k);
+        return maxSlidingWindow_2(nums, k);
+    }
+
+    /**
+     * review
+     * AC中更优的耗时13ms的实现
+     * https://leetcode.com/problems/sliding-window-maximum/discuss/65881/O(n)-solution-in-Java-with-two-simple-pass-in-the-array
+     * 太笨了没理解
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] maxSlidingWindow_3(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0) return new int[0];
+        if (k == 1) return nums;
+
+        int[] left = new int[n];
+        left[0] = nums[0];
+        int[] right = new int[n];
+        right[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            // from left to right
+            if (i % k == 0) left[i] = nums[i];  // block_start
+            else left[i] = Math.max(left[i - 1], nums[i]);
+
+            // from right to left
+            int j = n - i - 1;
+            if ((j + 1) % k == 0) right[j] = nums[j];  // block_end
+            else right[j] = Math.max(right[j + 1], nums[j]);
+        }
+
+        int[] output = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++)
+            output[i] = Math.max(left[i + k - 1], right[i]);
+
+        return output;
+    }
+
+    /**
+     * review
+     * 参考思路：
+     * https://leetcode.com/problems/sliding-window-maximum/discuss/65884/Java-O(n)-solution-using-deque-with-explanation
+     *
+     * 非常巧妙的思路。
+     * 1.通过使用提前去掉不满足条件的数字，使得Deque中的数字一定是局部最优(到i为止)解空间中的。
+     * 2.使用了排除法，先去掉一定不在解空间中的数字。
+     *
+     * 思路：
+     * 1.不在滑动窗口内的要排除
+     * 2.如果n[i]<n[a]并且i<a，那么n[i]一定不满足条件
+     * 3.用Dequeue或者LinkedList保存窗口数据的下标
+     * 4.Dequeue中的第一个数字就是当前窗口的最大值
+     *
+     * 验证通过：
+     * Runtime: 98 ms, faster than 20.96% of Java online submissions for Sliding Window Maximum.
+     * Memory Usage: 143.7 MB, less than 72.97% of Java online submissions for Sliding Window Maximum.
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] maxSlidingWindow_2(int[] nums, int k) {
+        if (nums == null || k <= 0) {
+            return new int[0];
+        }
+        int[] res = new int[nums.length - k + 1];
+        int idx = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (!queue.isEmpty() && queue.peekFirst() <= i - k) {
+                queue.pollFirst();
+            }
+            while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
+                queue.pollLast();
+            }
+            queue.offer(i);
+            if (k - 1 <= i) {
+                res[idx++] = nums[queue.peek()];
+            }
+        }
+        return res;
     }
 
     /**
