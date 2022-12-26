619ca2797371ebaf1038a0202bf719a4c4aa5ca1
liuyanhui414 <liuyanhui414@gmail.com>
2022-12-26 09:29:24 +0800

    round 2 , modify 306

--- src/main/java/leetcode/Additive_Number_306.java
+++ src/main/java/leetcode/Additive_Number_306.java
@@ -28,6 +28,86 @@ package leetcode;
  * How would you handle overflow for very large input integers?
  */
 public class Additive_Number_306 {
+    public static boolean isAdditiveNumber(String num) {
+        return isAdditiveNumber_2(num);
+    }
+
+    /**
+     * round 2
+     * 思考：
+     * 1.先分割，再计算
+     * 2.分析输入"12358"、"123581321349"和"199100199"、"199100200"可以发现，只要不满足条件，那么就必须退回到最开通的两个数相加，重新开始查找和计算。即如果子序列为{a,b,c,d,e,f}被验证为失败，那么必须从重新选择a和b开始从头计算。那么问题就转化为：先修改b，再修改a，即先增加b，如果仍然失败，那么再增加a。可以使用递归法，F(a,b,remain_str)
+     * 3.递归算法。
+     *
+     * 递归算法：
+     * 1.设递归函数为F(a,b,num)。
+     * 2.如果a+b==int(num)，那么返回true；如果a+b==c存在且remain_str不为空，那么F(b,c,remain_str)；如果a+b==c不存在，返回false。
+     * 3.在函数主入口，通过组合不同的a和b，然后调用F(a,b,num)
+     *
+     * 改进思路：把int改成long进行比较和计算
+     *
+     * 验证通过：
+     * Runtime 2 ms Beats 65.19%
+     * Memory 41.9 MB Beats 56.10%
+     * @param num
+     * @return
+     */
+    public static boolean isAdditiveNumber_2(String num) {
+        for (int i = 1; i < num.length(); i++) {
+            String a = num.substring(0, i);
+            for (int j = i + 1; j < num.length(); j++) {
+                String b = num.substring(i, j);
+                if (helper(a, b, num.substring(j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean helper(String a, String b, String num) {
+        //过滤前导0
+        if ((a.charAt(0) == '0' && a.length() > 1)
+                || (b.charAt(0) == '0' && b.length() > 1)) return false;
+        if (num == null || num.length() == 0) return true;
+        //查找sum
+        for (int i = 1; i <= num.length(); i++) {
+            String c = num.substring(0, i);
+            String sum = add2Str(a, b);
+            if (sum.equals(c) && helper(b, c, num.substring(i))) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private static String add2Str(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1, j = b.length() - 1;
+        while (i >= 0 && j >= 0) {
+            int t = (a.charAt(i--) - '0') + (b.charAt(j--) - '0') + carry;
+            sb.append(t % 10);
+            carry = t >= 10 ? 1 : 0;
+        }
+        while (i >= 0) {
+            int t = (a.charAt(i--) - '0') + carry;
+            sb.append(t % 10);
+            carry = t >= 10 ? 1 : 0;
+        }
+        while (j >= 0) {
+            int t = (b.charAt(j--) - '0') + carry;
+            sb.append(t % 10);
+            carry = t >= 10 ? 1 : 0;
+        }
+
+        if (carry > 0)
+            sb.append("1");
+
+        return sb.reverse().toString();
+    }
+
     /**
      * 参考思路：
      * https://leetcode.com/problems/additive-number/discuss/75576/0ms-concise-C%2B%2B-solution-(perfectly-handles-the-follow-up-and-leading-0s)
@@ -41,7 +121,7 @@ public class Additive_Number_306 {
      * @param num
      * @return
      */
-    public static boolean isAdditiveNumber(String num) {
+    public static boolean isAdditiveNumber_1(String num) {
         if (num.length() < 3) return false;
         for (int i = 1; i <= num.length() / 2; i++) {
             for (int j = i + 1; j < num.length(); j++) {
@@ -56,7 +136,7 @@ public class Additive_Number_306 {
     private static boolean check(String a, String b, String num) {
         if (num == null || num.equals("")) return true;
         //去掉b是0开头，位数大于1的情况，即033,0033等情况
-        if (b.startsWith("0") && b.length()>1) return false;
+        if (b.startsWith("0") && b.length() > 1) return false;
         String c = addTwoNum(a, b);
         if (num.startsWith(c)) {
             return check(b, c, num.substring(c.length()));
@@ -104,12 +184,16 @@ public class Additive_Number_306 {
         do_func("101", true);
         do_func("199001990", true);
         do_func("100010", false);
+        do_func("12358132134", true);
+        do_func("123581321340", false);
+        do_func("000", true);
+        do_func("000000", true);
     }
 
     private static void do_func(String num, boolean expected) {
         boolean ret = isAdditiveNumber(num);
         System.out.println(ret);
-        System.out.println(ret == expected);
+        System.out.println(ret == expected ? "√" : "X");
         System.out.println("--------------");
     }
 }
