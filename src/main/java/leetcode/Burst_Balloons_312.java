0f2643ebbc7beb60184c551817edc31432d9321a
liuyanhui414 <liuyanhui414@gmail.com>
2023-03-25 15:28:08 +0800

    modify 312,add DP solution

--- src/main/java/leetcode/Burst_Balloons_312.java
+++ src/main/java/leetcode/Burst_Balloons_312.java
@@ -31,9 +31,69 @@ import java.util.Map;
  */
 public class Burst_Balloons_312 {
     public static int maxCoins(int[] nums) {
-        return maxCoins_2(nums);
+        return maxCoins_3(nums);
     }
 
+    /**
+     * review
+     * 参考文档：
+     * https://leetcode.com/problems/burst-balloons/solutions/76228/share-some-analysis-and-explanations/
+     *
+     * DP 思路，在递归思路基础上而来。毕竟递归是自顶向下，DP是自底向上。
+     * 需要根据递归算法推导出DP过程。从maxCoins_2的递归发可以得出，最后一层递归是1个气球burst，第二层是2个气球burst...
+     * 那么DP需要模拟这个过程。即先计算1个气球burst，计算2个气球burst，计算3个气球burst....， 计算n个气球burst
+     *
+     * 验证通过：
+     * Runtime 47 ms Beats 78.22%
+     * Memory 42.4 MB Beats 45.75%
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxCoins_3(int[] nums) {
+        // 创建一个辅助数组，并在首尾各添加1，方便处理边界情况
+        // 有了这个辅助数据，DP使用开区间的方式保存数据
+        int[] temp = new int[nums.length + 2];
+        int t = 1;
+        temp[0] = 1;
+        temp[temp.length - 1] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            temp[t++] = nums[i];
+        }
+        //dp[i][j]为开区间方式
+        int[][] dp = new int[temp.length][temp.length];
+        //每次计算的开区间的长度
+        for (int len = 3; len <= temp.length; len++) {
+            for (int left = 0; left <= temp.length - len; left++) {
+                int right = left + len - 1;
+                //开区间内进行计算
+                for (int i = left + 1; i <= right - 1; i++) {
+                    //要注意下面的"temp[left] * temp[i] * temp[right]"，不能写成"temp[i-1] * temp[i] * temp[i+1]"
+                    dp[left][right] = Math.max(dp[left][right],
+                            temp[left] * temp[i] * temp[right] + dp[left][i] + dp[i][right]);
+                }
+            }
+        }
+
+        return dp[0][temp.length - 1];
+    }
+
+    /**
+     * review
+     * 参考文档：
+     * https://leetcode.com/problems/burst-balloons/solutions/76228/share-some-analysis-and-explanations/
+     *
+     * Divide and Conquer 算法：
+     * 1.采用分治法Divide and Conquer
+     * 2.对每一个nums[i]，要么最先选，要么最后选。如果最先选，因为nums[i]最先选了之后，nums还是一个数组，无法使用分治法。所以nums[i]只能最后选。换一个角度，如果nums[i]最先选，那么会影响nums[i-1]和nums[i+1]就变成相邻的了，按现有分治法的思路就会丢失这种情况。简单来说如果最先选nums[i]会对其他分治的结果产生影响。
+     * 3.分治+缓存法
+     *
+     * 验证通过：
+     * Runtime 130 ms Beats 39.36%
+     * Memory 42.8 MB Beats 19.1%
+     * @param nums
+     * @return
+     */
     public static int maxCoins_2(int[] nums) {
         int[] newNums = new int[nums.length + 2];
         int t = 1;
