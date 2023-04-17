e94d308a3e44b2f2da06d4b75a93f6d6b6410be5
liuyanhui414 <liuyanhui414@gmail.com>
2023-04-17 16:03:45 +0800

    round 2 ,modify 332,reivew

--- src/main/java/leetcode/Reconstruct_Itinerary_332.java
+++ src/main/java/leetcode/Reconstruct_Itinerary_332.java
@@ -5,12 +5,14 @@ import java.util.*;
 /**
  * 332. Reconstruct Itinerary
  * Medium
+ * Hard（Change To Hard level Since Round 2）
  * -----------------------
  * You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
  *
  * All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
  *
  * For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+ *
  * You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
  *
  * Example 1:
@@ -33,11 +35,17 @@ import java.util.*;
 public class Reconstruct_Itinerary_332 {
 
     public static List<String> findItinerary(List<List<String>> tickets) {
-        return findItinerary_4(tickets);
+        return findItinerary_3(tickets);
     }
 
     /**
-     * 金矿
+     * review round 2
+     * 1.动态排序可以是直接使用PriorityQueue
+     * 2.PriorityQueue中存在重复数据时，删除操作无法精准操作。
+     */
+
+    /**
+     * review 金矿
      * 欧拉通路问题，一笔画问题
      * 参考思路：
      * https://leetcode-cn.com/problems/reconstruct-itinerary/solution/zhong-xin-an-pai-xing-cheng-by-leetcode-solution/
@@ -65,9 +73,11 @@ public class Reconstruct_Itinerary_332 {
     }
 
     private static void dfs(Map<String, PriorityQueue<String>> targets, String cur, Stack<String> path) {
+        //round 2 : 逆序入栈。所以这里先从lexical最大的开始遍历。这样就避免了PriorityQueue.remove()操作中重复数据删除的问题。见findItinerary_2()。从一边删除有问题时，那就转变思路从另一边开始删除。
         while (targets.containsKey(cur) && targets.get(cur).size() > 0) {
             dfs(targets, targets.get(cur).poll(), path);
         }
+        //round 2 : 逆序入栈
         path.push(cur);
     }
 
@@ -142,6 +152,8 @@ public class Reconstruct_Itinerary_332 {
      * 3.当路径中节点数<tc+1且遍历结束时，回退并按照次小字母序生成新路径
      * 4.当路径中节点数<tc+1且遍历未结束时，递归调用
      *
+     * review round2 这个坑round2又踩了，说明这种方式有个很大的问题。PriorityQueue中存在重复数据时，删除操作无法精准操作。
+     *
      * 验证失败，有用例未通过。26 / 80 test cases passed.
      * 问题出现在递归方法中PriorityQueue遍历的部分。具体原因见代码注释。findItinerary_4的方法通过了验证。
      *
@@ -183,6 +195,7 @@ public class Reconstruct_Itinerary_332 {
             path.add(nodeArr[i]);
             //金矿
             //TODO 这里是问题所在，可能remove了错误的元素。可能存在重复的元素。如果是被remove的元素在之前存在重复的值，这个remove会错误的删除前面的哪个相等的值，从而导致计算结果错误。
+            //review round2 这个坑round2又踩了，说明这种方式有个很大的问题。PriorityQueue中存在重复数据时，删除操作无法精准操作。
             queue.remove(nodeArr[i]);
             if (do_find_recursive(map, tc, path)) {
                 return true;
@@ -275,6 +288,10 @@ public class Reconstruct_Itinerary_332 {
         do_func(new String[][]{{"JFK", "SFO"}, {"SFO", "JFK"}, {"JFK", "SFO"}, {"SFO", "JFK"}}, new String[]{"JFK", "SFO", "JFK", "SFO", "JFK"});
         do_func(new String[][]{{"JFK", "KUL"}, {"JFK", "NRT"}, {"NRT", "JFK"}}, new String[]{"JFK", "NRT", "JFK", "KUL"});
         do_func(new String[][]{{"EZE", "TIA"}, {"EZE", "HBA"}, {"AXA", "TIA"}, {"JFK", "AXA"}, {"ANU", "JFK"}, {"ADL", "ANU"}, {"TIA", "AUA"}, {"ANU", "AUA"}, {"ADL", "EZE"}, {"ADL", "EZE"}, {"EZE", "ADL"}, {"AXA", "EZE"}, {"AUA", "AXA"}, {"JFK", "AXA"}, {"AXA", "AUA"}, {"AUA", "ADL"}, {"ANU", "EZE"}, {"TIA", "ADL"}, {"EZE", "ANU"}, {"AUA", "ANU"}}, new String[]{"JFK", "AXA", "AUA", "ADL", "ANU", "AUA", "ANU", "EZE", "ADL", "EZE", "ANU", "JFK", "AXA", "EZE", "TIA", "AUA", "AXA", "TIA", "ADL", "EZE", "HBA"});
+        do_func(new String[][]{{"JFK", "KUL"}}, new String[]{"JFK", "KUL"});
+        do_func(new String[][]{{"AXA", "EZE"}, {"EZE", "AUA"}, {"ADL", "JFK"}, {"ADL", "TIA"}, {"AUA", "AXA"}, {"EZE", "TIA"}, {"EZE", "TIA"}, {"AXA", "EZE"}, {"EZE", "ADL"}, {"ANU", "EZE"}, {"TIA", "EZE"}, {"JFK", "ADL"}, {"AUA", "JFK"}, {"JFK", "EZE"}, {"EZE", "ANU"}, {"ADL", "AUA"}, {"ANU", "AXA"}, {"AXA", "ADL"}, {"AUA", "JFK"}, {"EZE", "ADL"}, {"ANU", "TIA"}, {"AUA", "JFK"}, {"TIA", "JFK"}, {"EZE", "AUA"}, {"AXA", "EZE"}, {"AUA", "ANU"}, {"ADL", "AXA"}, {"EZE", "ADL"}, {"AUA", "ANU"}, {"AXA", "EZE"}, {"TIA", "AUA"}, {"AXA", "EZE"}, {"AUA", "SYD"}, {"ADL", "JFK"}, {"EZE", "AUA"}, {"ADL", "ANU"}, {"AUA", "TIA"}, {"ADL", "EZE"}, {"TIA", "JFK"}, {"AXA", "ANU"}, {"JFK", "AXA"}, {"JFK", "ADL"}, {"ADL", "EZE"}, {"AXA", "TIA"}, {"JFK", "AUA"}, {"ADL", "EZE"}, {"JFK", "ADL"}, {"ADL", "AXA"}, {"TIA", "AUA"}, {"AXA", "JFK"}, {"ADL", "AUA"}, {"TIA", "JFK"}, {"JFK", "ADL"}, {"JFK", "ADL"}, {"ANU", "AXA"}, {"TIA", "AXA"}, {"EZE", "JFK"}, {"EZE", "AXA"}, {"ADL", "TIA"}, {"JFK", "AUA"}, {"TIA", "EZE"}, {"EZE", "ADL"}, {"JFK", "ANU"}, {"TIA", "AUA"}, {"EZE", "ADL"}, {"ADL", "JFK"}, {"ANU", "AXA"}, {"AUA", "AXA"}, {"ANU", "EZE"}, {"ADL", "AXA"}, {"ANU", "AXA"}, {"TIA", "ADL"}, {"JFK", "ADL"}, {"JFK", "TIA"}, {"AUA", "ADL"}, {"AUA", "TIA"}, {"TIA", "JFK"}, {"EZE", "JFK"}, {"AUA", "ADL"}, {"ADL", "AUA"}, {"EZE", "ANU"}, {"ADL", "ANU"}, {"AUA", "AXA"}, {"AXA", "TIA"}, {"AXA", "TIA"}, {"ADL", "AXA"}, {"EZE", "AXA"}, {"AXA", "JFK"}, {"JFK", "AUA"}, {"ANU", "ADL"}, {"AXA", "TIA"}, {"ANU", "AUA"}, {"JFK", "EZE"}, {"AXA", "ADL"}, {"TIA", "EZE"}, {"JFK", "AXA"}, {"AXA", "ADL"}, {"EZE", "AUA"}, {"AXA", "ANU"}, {"ADL", "EZE"}, {"AUA", "EZE"}}, new String[]{"JFK", "ADL", "ANU", "ADL", "ANU", "AUA", "ADL", "AUA", "ADL", "AUA", "ANU", "AXA", "ADL", "AUA", "ANU", "AXA", "ADL", "AXA", "ADL", "AXA", "ANU", "AXA", "ANU", "AXA", "EZE", "ADL", "AXA", "EZE", "ADL", "AXA", "EZE", "ADL", "EZE", "ADL", "EZE", "ADL", "EZE", "ANU", "EZE", "ANU", "EZE", "AUA", "AXA", "EZE", "AUA", "AXA", "EZE", "AUA", "AXA", "JFK", "ADL", "EZE", "AUA", "EZE", "AXA", "JFK", "ADL", "JFK", "ADL", "JFK", "ADL", "JFK", "ADL", "TIA", "ADL", "TIA", "AUA", "JFK", "ANU", "TIA", "AUA", "JFK", "AUA", "JFK", "AUA", "TIA", "AUA", "TIA", "AXA", "TIA", "EZE", "AXA", "TIA", "EZE", "JFK", "AXA", "TIA", "EZE", "JFK", "AXA", "TIA", "JFK", "EZE", "TIA", "JFK", "EZE", "TIA", "JFK", "TIA", "JFK", "AUA", "SYD"});
+
+
     }
 
     private static void do_func(String[][] tickets, String[] expected) {
