a49be5fc633f4d9f788a3f6bc23d5d13181a69bd
liuyanhui414 <liuyanhui414@gmail.com>
2023-04-24 11:40:47 +0800

    round 2 , moidfy 337

--- src/main/java/leetcode/House_Robber_III_337.java
+++ src/main/java/leetcode/House_Robber_III_337.java
@@ -31,6 +31,44 @@ public class House_Robber_III_337 {
     private static Map<TreeNode, Integer> cached = new HashMap<>();
 
     /**
+     * round 2
+     *
+     * Thinking：
+     * 1.本题具有迷惑性，虽然以树的形式出现，但是未必采用树的解法。也有可能是图的解法。并一定从root开始计算，root只是一个节点而已。
+     * 2.如果父节点被选中，子节点就无法被选中，反之亦然。所以可以用DFS+后序遍历法。每个子树有两种状态，root选中和root未选中。
+     * 每个节点都维护两个值：选中和未选中。
+     * 公式为：
+     * F(node,选中)=node.val+F(node.left,未选中)+F(node.right,未选中)
+     * F(node,未选中)=max(F(node.left,选中),F(node.left,未选中))+max(F(node.right,选中),F(node.right,未选中))
+     * 最终结果为：
+     * res=max(F(root,选中),F(root,未选中))
+     *
+     * 验证通过：
+     * Runtime 0 ms Beats 100%
+     * Memory 42.2 MB Beats 72.76%
+     *
+     * @param root
+     * @return
+     */
+    public int rob_2(TreeNode root) {
+        int[] t = dfs(root);
+        return Math.max(t[0], t[1]);
+    }
+
+    private int[] dfs(TreeNode node) {
+        // res[0]未选中；res[1]选中
+        int[] res = new int[2];
+        if (node == null) return res;
+        int[] l_res = dfs(node.left);
+        int[] r_res = dfs(node.right);
+        //未选中
+        res[0] = Math.max(l_res[0], l_res[1]) + Math.max(r_res[0], r_res[1]);
+        //选中
+        res[1] = node.val + l_res[0] + r_res[0];
+        return res;
+    }
+
+    /**
      * 递归法。
      * 关键在于总结出公式。将中间结果缓存起来，防止重复计算，导致Time Limit Exceeded。
      *
