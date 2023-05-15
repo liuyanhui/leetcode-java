b92981becedff593504d6cb0de07c7e84e7cb6dd
liuyanhui414 <liuyanhui414@gmail.com>
2023-05-15 15:50:53 +0800

    add 363

--- src/main/java/leetcode/Max_Sum_of_Rectangle_No_Larger_Than_K_363.java
+++ src/main/java/leetcode/Max_Sum_of_Rectangle_No_Larger_Than_K_363.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.TreeSet;
+
 /**
  * 363. Max Sum of Rectangle No Larger Than K
  * Hard
@@ -28,13 +30,81 @@ package leetcode;
  */
 public class Max_Sum_of_Rectangle_No_Larger_Than_K_363 {
     public static int maxSumSubmatrix(int[][] matrix, int k) {
-        return maxSumSubmatrix_2(matrix, k);
+        return maxSumSubmatrix_3(matrix, k);
     }
 
     /**
+     * review 有种更巧妙的方法，在maxSumSubmatrix_2()基础上进行了优化
+     *
+     * 参考资料：
+     * https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/solutions/83599/accepted-c-codes-with-explanation-and-references/
+     * https://www.quora.com/Given-an-array-of-integers-A-and-an-integer-k-find-a-subarray-that-contains-the-largest-sum-subject-to-a-constraint-that-the-sum-is-less-than-k
+     * https://leetcode.cn/problems/max-sum-of-rectangle-no-larger-than-k/solution/ju-xing-qu-yu-bu-chao-guo-k-de-zui-da-sh-70q2/
+     *
+     * 思路：
+     * 1.问题分为两个部分：计算rectangle的面积；判断面积是否为不大于k的最大值。
+     * 2.计算rectangle的面积通过枚举矩形的上下边界，并计算出该边界内每列的元素和的方式解决。这部分的复杂度是O(M*N*M)，参考maxSumSubmatrix_2()
+     * 3.在2.的基础上问题转化成：给定一个整数数组和一个整数k，计算该数组的最大区间和，要求区间和不超过k。 参考下面的分析：
+     *  定义长度为 n 的数组 a 的前缀和:
+     *  PrefixSum[0]=0
+     *  PrefixSum[1]=nums[1]
+     *  PrefixSum[2]=nums[2]+PrefixSum[1]
+     *  ...
+     *  PrefixSum[i]=nums[i]+PrefixSum[i-1]
+     *
+     *  区间[l,r)的和表示为PrefixSum[r]-PrefixSum[l]。
+     *  问题的条件转化成PrefixSum[r]-PrefixSum[l]<=k，即查找PrefixSum[r]-PrefixSum[l]的最大值，且该值小于等于k。
+     *  这个不等式中有两个变量r和l。我们通过枚举r将其转化（降维）为一个变量的问题。
+     *  在r确定的情况下（枚举r时，PrefixSum[r]是确定的值），不等式转化为PrefixSum[r]-k<=PrefixSum[l]，这时我们只需要找到满足条件的l。思路如下：由于l<r，所以枚举r时,PrefixSum[l]已经计算过了；我们把PrefixSum[l]保存在有序集合S中；在S中查找大于等于PrefixSum[r]-k的最小值。
+     *
+     * 验证通过：
+     * Runtime 422 ms Beats 16.41%
+     * Memory 42.5 MB Beats 82.56%
+     *
+     * @param matrix
+     * @param k
+     * @return
+     */
+    public static int maxSumSubmatrix_3(int[][] matrix, int k) {
+        if (matrix.length == 0) return 0;
+        int res = Integer.MIN_VALUE;
+        for (int col0 = 0; col0 < matrix[0].length; col0++) {
+            int[] rows = new int[matrix.length];
+            for (int col1 = col0; col1 < matrix[0].length; col1++) {
+                //两侧列确定的情况下计算没行的sum，利用了前缀和思路
+                for (int i = 0; i < matrix.length; i++) {
+                    rows[i] += matrix[i][col1];
+                }
+                TreeSet<Integer> treeSet = new TreeSet<>();
+                treeSet.add(0);
+                int r = 0;
+                for (int n : rows) {
+                    r += n;
+                    //FIXME 这里很巧妙，查询满足条件r-k<=l时，l的最小值
+                    //TreeSet.ceiling(E e) 方法用于返回此集合中大于或等于给定元素的最小元素，如果没有这样的元素，则返回 null。
+                    Integer ceiling = treeSet.ceiling(r - k);
+                    if (ceiling != null) {
+                        res = Math.max(res, r - ceiling);
+                    }
+                    treeSet.add(r);
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 参考资料：
+     * https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/solutions/83599/accepted-c-codes-with-explanation-and-references/
+     * https://leetcode.cn/problems/max-sum-of-rectangle-no-larger-than-k/solution/javacong-bao-li-kai-shi-you-hua-pei-tu-pei-zhu-shi/
+     *
      * 与maxSumSubmatrix_1()思路略有不同
      * 1.划分左右边界，并求出在此边界下，每行的总和
      * 2.暴力求解不大于k的最大值
+     *
+     *
+     * Time Complexity:O(M*M*N*N)
+     * Space Complexity:O(M*N)
      *
      * 验证通过：
      * Runtime 237 ms Beats 84.69%
@@ -53,7 +123,7 @@ public class Max_Sum_of_Rectangle_No_Larger_Than_K_363 {
                 for (int i = 0; i < matrix.length; i++) {
                     rows[i] += matrix[i][col1];
                 }
-
+                //FIXME maxSumSubmatrix_3()对下面的代码进行了优化
                 for (int i = 0; i < rows.length; i++) {
                     int sum = 0;
                     for (int j = i; j < rows.length; j++) {
---
+++ src/main/java/leetcode/Subarray_Sum_Equals_K_560.java
@@ -0,0 +1,4 @@
+package leetcode;
+
+public class Subarray_Sum_Equals_K_560 {
+}
