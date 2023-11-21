1db59b373a1c4ab5ae8208b6158386c53c422553
liuyanhui414 <liuyanhui414@gmail.com>
2023-11-21 16:36:49 +0800

    round 3 ,modify 31

--- src/main/java/leetcode/Next_Permutation_31.java
+++ src/main/java/leetcode/Next_Permutation_31.java
@@ -31,6 +31,57 @@ package leetcode;
  * 0 <= nums[i] <= 100
  */
 public class Next_Permutation_31 {
+    public static void nextPermutation(int[] nums) {
+        nextPermutation_2(nums);
+    }
+
+    /**
+     * round 3
+     * Score[2] Lower is harder
+     *
+     * Thinking：
+     * 1.该问题包含两个主要部分：计算出排列，比较大小
+     * 2.naive solution
+     * 2.1.计算出所有的排列，排序排列，查找符合要求的结果。
+     * 2.2.计算排列时，每个元素有三种情况：先计算(在结果的第一位），后计算（在结果的最后一位），不计算（在结果的中间）
+     * 3.根据当前输入和某种规则计算出结果。分析出输入值的下一个值的规律。需要三步
+     * 3.1.从后向前查找出第一个递减的趋势。
+     * 3.2.反转nums[i:~]。（因为初始时nums[i:~]是单调不减的）
+     * 3.3.在反转后的nums[i:~]找个第一个大于nums[i-1]的数，并与nums[i-1]交换。
+     *
+     * 验证通过：
+     * Runtime 0ms Beats 100.00%of users with Java
+     * Memory 42.20MB Beats 28.72%of users with Java
+     *
+     * @param nums
+     */
+    public static void nextPermutation_2(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        //从右向左查找第一个递减趋势
+        int i = nums.length - 1;
+        while (i > 0) {
+            if (nums[i - 1] < nums[i]) break;
+            i--;
+        }
+        //反转nums[i:~]
+        int l = i, r = nums.length - 1;
+        while (l < r) {
+            swap(nums, l, r);
+            l++;
+            r--;
+        }
+        if (i == 0) return;
+        //在反转后的nums[i:~]中找到第一个大于nums[i-1]的数
+        int j = i;
+        for (; j < nums.length; j++) {
+            //找个第一个大于nums[i-1]的数，并与nums[i-1]交换
+            if (nums[i - 1] < nums[j]) {
+                swap(nums, i - 1, j);
+                break;
+            }
+        }
+    }
+
     /**
      * round2
      * 关键：分析出算法模型
@@ -44,7 +95,7 @@ public class Next_Permutation_31 {
      *
      * @param nums
      */
-    public static void nextPermutation(int[] nums) {
+    public static void nextPermutation_1(int[] nums) {
         int i = nums.length - 1, j = nums.length - 1;
         //从后向前查找，找到第一个单调减少的元素nums[i]
         while (i > 0 && nums[i - 1] >= nums[i--]) ;
@@ -82,7 +133,7 @@ public class Next_Permutation_31 {
 
     private static void do_func(int[] nums, int[] expected) {
         nextPermutation(nums);
-        ArrayUtils.printIntArray(nums);
+        ArrayUtils.printlnIntArray(nums);
         ArrayUtils.isSameThenPrintln(nums, expected);
         System.out.println("--------------");
     }
