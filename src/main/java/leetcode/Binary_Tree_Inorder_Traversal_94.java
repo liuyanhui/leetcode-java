1394c4930eeefcdeb6c8cc30198482aec811681c
liuyanhui414 <liuyanhui414@gmail.com>
2024-04-10 14:52:58 +0800

    round 3 , modify 94

--- src/main/java/leetcode/Binary_Tree_Inorder_Traversal_94.java
+++ src/main/java/leetcode/Binary_Tree_Inorder_Traversal_94.java
@@ -30,7 +30,42 @@ import java.util.Stack;
  */
 public class Binary_Tree_Inorder_Traversal_94 {
     public List<Integer> inorderTraversal(TreeNode root) {
-        return inorderTraversal_5(root);
+        return inorderTraversal_6(root);
+    }
+
+    /**
+     * round 3
+     * Score[2] Lower is harder
+     *
+     * review inorderTraversal_4()
+     * https://zhuanlan.zhihu.com/p/102285533
+     *
+     * @param root
+     * @return
+     */
+    public static List<Integer> inorderTraversal_6(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left != null) {
+                TreeNode t = cur.left;
+                //获取左子树的最右节点
+                while (t.right != null && t.right != cur) {
+                    t = t.right;
+                }
+                if (t.right == null) {//第一次访问，搭建临时路径，生成一个环
+                    t.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {//第二次访问，删除临时路径，破坏环，恢复原状
+                    t.right = null;
+                }
+            }
+            ret.add(cur.val);
+            cur = cur.right;
+        }
+
+        return ret;
     }
 
     /**
@@ -42,6 +77,8 @@ public class Binary_Tree_Inorder_Traversal_94 {
      * 2. Tree是特殊的Graph。
      * 3. review 采用栈+循环，也可以采用递归。重点是出栈后的逻辑要覆盖各种情况。
      * 3.1. 出栈的节点node，只需要执行两步：当前节点node.val加入结果集；node.right入栈；node.right的所有的左子孙节点都入栈
+     *
+     * review inorderTraversal_3()是更优雅的迭代实现
      *
      * 验证通过：
      * Runtime 0 ms Beats 100.00%
@@ -77,8 +114,15 @@ public class Binary_Tree_Inorder_Traversal_94 {
     }
 
     /**
-     * 金矿：
+     * review 金矿：
      * Threaded Binary Tree，or Morris Traversal
+     * 1.在O(1)空间复杂度和O(N)时间复杂度约束下，实现binary Tree的遍历
+     * 2.临时搭建一个回溯的路径，遍历完成后，再删除临时搭建的回溯路径。每个节点都需要搭建临时的回溯路径。
+     * 3.inorder：为节点的左子树的最右子节点搭建临时路径；【可选】如果当前节点有左子节点，删除previous节点的回溯路径（其右子节点路径），把Tree恢复原状。
+     * 3.1. 判断是否第一次访问节点的逻辑是：是否存在环。第一次访问时，环是不不存在的。第一次访问且满足条件时，才会产生一个环。
+     * 3.2. 第一次访问节点时，恢复原状，打破环。
+     *
+     * 本实现，没有删除临时路径，把TreeNode恢复原状。
      *
      * 参考思路：
      * https://leetcode.com/problems/binary-tree-inorder-traversal/solution/
@@ -116,7 +160,7 @@ public class Binary_Tree_Inorder_Traversal_94 {
     }
 
     /**
-     * 迭代
+     * review 优雅的迭代实现
      *
      * 参考资料：
      * https://leetcode.com/problems/binary-tree-inorder-traversal/solution/
@@ -132,6 +176,7 @@ public class Binary_Tree_Inorder_Traversal_94 {
         List<Integer> res = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
         TreeNode curr = root;
+        //下面的判断条件"curr != null"很关键
         while (curr != null || !stack.isEmpty()) {
             while (curr != null) {
                 stack.push(curr);
