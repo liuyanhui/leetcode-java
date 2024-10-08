0e9619a0b79936236174599750b0a838cc315e0c
lyhLenovo <liuyanhui414@163.com>
2024-10-08 09:10:01 +0800

    round 3 , modify 191

--- src/main/java/leetcode/Number_of_1_Bits_191.java
+++ src/main/java/leetcode/Number_of_1_Bits_191.java
@@ -5,29 +5,29 @@ package leetcode;
  * Easy
  * --------------------------
  * Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
- *
+ * <p>
  * Note:
  * Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
  * In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
- *
+ * <p>
  * Example 1:
  * Input: n = 00000000000000000000000000001011
  * Output: 3
  * Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
- *
+ * <p>
  * Example 2:
  * Input: n = 00000000000000000000000010000000
  * Output: 1
  * Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
- *
+ * <p>
  * Example 3:
  * Input: n = 11111111111111111111111111111101
  * Output: 31
  * Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
- *
+ * <p>
  * Constraints:
  * The input must be a binary string of length 32.
- *
+ * <p>
  * Follow up: If this function is called many times, how would you optimize it?
  */
 public class Number_of_1_Bits_191 {
@@ -36,8 +36,27 @@ public class Number_of_1_Bits_191 {
     }
 
     /**
-     * Bit Manipulation
+     * round 3
+     * Score[5] Lower is harder
+     * <p>
+     * 验证通过：
+     * Runtime 0 ms Beats 100.00%
+     * Memory 40.90 MB Beats 14.78%
      *
+     * @param n
+     * @return
+     */
+    public static int hammingWeight_r3_1(int n) {
+        int res = 0;
+        for(int i=0;i<32;i++){
+            res += n>>i & 1;
+        }
+        return res;
+    }
+
+    /**
+     * Bit Manipulation
+     * <p>
      * 验证通过：
      * Runtime: 1 ms, faster than 85.79% of Java online submissions for Number of 1 Bits.
      * Memory Usage: 41.3 MB, less than 46.92% of Java online submissions for Number of 1 Bits.
