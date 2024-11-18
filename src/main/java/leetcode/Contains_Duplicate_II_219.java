73d88cd0a47763460e187980d4952b85453df8ca
lyhLenovo <liuyanhui414@163.com>
2024-11-18 10:03:14 +0800

    round 3 ,modify 219

--- src/main/java/leetcode/Contains_Duplicate_II_219.java
+++ src/main/java/leetcode/Contains_Duplicate_II_219.java
@@ -10,19 +10,19 @@ import java.util.Set;
  * Easy
  * ---------------
  * Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
- *
+ * <p>
  * Example 1:
  * Input: nums = [1,2,3,1], k = 3
  * Output: true
- *
+ * <p>
  * Example 2:
  * Input: nums = [1,0,1,1], k = 1
  * Output: true
- *
+ * <p>
  * Example 3:
  * Input: nums = [1,2,3,1,2,3], k = 2
  * Output: false
- *
+ * <p>
  * Constraints:
  * 1 <= nums.length <= 10^5
  * -10^9 <= nums[i] <= 10^9
@@ -31,12 +31,40 @@ import java.util.Set;
 public class Contains_Duplicate_II_219 {
 
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
-        return containsNearbyDuplicate_3(nums, k);
+        return containsNearbyDuplicate_r3_1(nums, k);
+    }
+
+    /**
+     * round 3
+     * Score[2] Lower is harder
+     *
+     * Thinking
+     * 1. naive solution
+     * 依次计算每个nums[i]，即依次从i开始向后查找
+     * Time Complexity:O(N*N)
+     * Space Complexity:O(1)
+     * 2. 先缓存每个数字的上一次出现的下标，再计算当前下标是否满足条件。
+     * Time Complexity:O(N)
+     * Space Complexity:O(N)
+     *
+     * 验证通过：
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static boolean containsNearbyDuplicate_r3_1(int[] nums, int k) {
+        Map<Integer, Integer> cache = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (cache.containsKey(nums[i]) && i - cache.get(nums[i]) <= k) return true;
+            cache.put(nums[i], i);
+        }
+        return false;
     }
 
     /**
      * round 2
-     *
+     * <p>
      * 验证通过：
      * Runtime: 42 ms, faster than 53.06% of Java online submissions for Contains Duplicate II.
      * Memory Usage: 93.5 MB, less than 53.66% of Java online submissions for Contains Duplicate II.
@@ -55,7 +83,6 @@ public class Contains_Duplicate_II_219 {
         }
         return false;
     }
-
 
     /**
      * 参考思路：
