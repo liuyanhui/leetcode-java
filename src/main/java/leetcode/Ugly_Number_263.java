a8d1db5bee2ba82f270da7a658d2f1b3b7b62ba2
lyhLenovo <liuyanhui414@163.com>
2024-12-27 09:06:21 +0800

    round 3 , modify 263

--- src/main/java/leetcode/Ugly_Number_263.java
+++ src/main/java/leetcode/Ugly_Number_263.java
@@ -6,42 +6,65 @@ package leetcode;
  * -------------------------
  * An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
  * Given an integer n, return true if n is an ugly number.
- *
+ * <p>
  * Example 1:
  * Input: n = 6
  * Output: true
  * Explanation: 6 = 2 × 3
- *
+ * <p>
  * Example 2:
  * Input: n = 8
  * Output: true
  * Explanation: 8 = 2 × 2 × 2
- *
+ * <p>
  * Example 3:
  * Input: n = 14
  * Output: false
  * Explanation: 14 is not ugly since it includes the prime factor 7.
- *
+ * <p>
  * Example 4:
  * Input: n = 1
  * Output: true
  * Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
- *
- *  Constraints:
+ * <p>
+ * Constraints:
  * -2^31 <= n <= 2^31 - 1
  */
 public class Ugly_Number_263 {
     public static boolean isUgly(int n) {
-        return isUgly_2(n);
+        return isUgly_r3_1(n);
+    }
+
+    /**
+     * round 3
+     * Score[5] Lower is harder
+     * <p>
+     * 验证通过
+     *
+     * @param n
+     * @return
+     */
+    public static boolean isUgly_r3_1(int n) {
+        if (n <= 0) return false;
+        while (n > 1 && n % 2 == 0) {
+            n /= 2;
+        }
+        while (n > 1 && n % 3 == 0) {
+            n /= 3;
+        }
+        while (n > 1 && n % 5 == 0) {
+            n /= 5;
+        }
+        return n == 1;
     }
 
     /**
      * round 2
      * 思考：
      * 1.暴力法。n依次循环与2,3,5做除法，直到无法整除。如果最终结果是1，返回true；否则返回false。
-     *
+     * <p>
      * isUgly_1()的实现更优
-     *
+     * <p>
      * 验证成功：
      * Runtime: 2 ms, faster than 75.69% of Java online submissions for Ugly Number.
      * Memory Usage: 41.7 MB, less than 21.38% of Java online submissions for Ugly Number.
@@ -67,6 +90,7 @@ public class Ugly_Number_263 {
      * 验证通过：
      * Runtime: 1 ms, faster than 100.00% of Java online submissions for Ugly Number.
      * Memory Usage: 36.1 MB, less than 42.04% of Java online submissions for Ugly Number.
+     *
      * @param n
      * @return
      */
@@ -89,13 +113,14 @@ public class Ugly_Number_263 {
         do_func(8, true);
         do_func(14, false);
         do_func(1, true);
-        do_func(0, true);
-        do_func(-1, true);
-        do_func(-2, true);
+        do_func(0, false);
+        do_func(-1, false);
+        do_func(-2, false);
         do_func(-21, false);
         do_func(7, false);
         do_func(Integer.MAX_VALUE, false);
         do_func(Integer.MIN_VALUE, false);
+        System.out.println("==================");
     }
 
     private static void do_func(int num, boolean expected) {
