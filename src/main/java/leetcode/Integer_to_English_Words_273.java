52c7b131a74f7a124b7f8e263356b15c9524cf10
lyhLenovo <liuyanhui414@163.com>
2025-01-02 11:57:04 +0800

    round 3 ,modify 273

--- src/main/java/leetcode/Integer_to_English_Words_273.java
+++ src/main/java/leetcode/Integer_to_English_Words_273.java
@@ -8,25 +8,102 @@ import java.util.List;
  * Hard
  * -----------------------------
  * Convert a non-negative integer num to its English words representation.
- *
+ * <p>
  * Example 1:
  * Input: num = 123
  * Output: "One Hundred Twenty Three"
- *
+ * <p>
  * Example 2:
  * Input: num = 12345
  * Output: "Twelve Thousand Three Hundred Forty Five"
- *
+ * <p>
  * Example 3:
  * Input: num = 1234567
  * Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
- *
+ * <p>
  * Constraints:
  * 0 <= num <= 2^31 - 1
  */
 public class Integer_to_English_Words_273 {
     public static String numberToWords(int num) {
-        return numberToWords_1(num);
+        return numberToWords_r3_1(num);
+    }
+
+    /**
+     * round 3
+     * Score[3] Lower is harder
+     * <p>
+     * Thinking
+     * 1. 分段法
+     * 1.1. 从右向左，每3个数字为一段，分别对应[billion,million,thousand]，最多4段
+     * 1.2. 每段最多3位数，分别是1位数、2位数、3位数。
+     * 1位数采用枚举；2位数时，分别对十位和个位采用枚举；3位数时，分别对百、个为采用枚举。
+     * 需要注意[10~19]需要单独考虑。
+     * 000时忽略该段
+     * 2. review 跟简洁的方法
+     * https://leetcode.com/problems/integer-to-english-words/solutions/70625/my-clean-java-solution-very-easy-to-understand/
+     * <p>
+     * 验证通过：
+     * Runtime 2 ms Beats 91.52%
+     * Memory 41.91 MB Beats 79.57%
+     *
+     * @param num
+     * @return
+     */
+    public static String numberToWords_r3_1(int num) {
+        if (num == 0) return "Zero";
+        //从右向左把输入划分为4段，每段3个数字
+        int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+        n1 = num % 1000;//thousand以内
+        n2 = num / 1000 % 1000;//thousand
+        n3 = num / 1000000 % 1000;//million
+        n4 = num / 1000000000;//billion
+        StringBuilder res = new StringBuilder();
+        String billion = getWords(n4);
+        if (billion != null && billion.length() != 0) {
+            res.append(billion + "Billion ");
+        }
+        String million = getWords(n3);
+        if (million != null && million.length() != 0) {
+            res.append(million + "Million ");
+        }
+        String thousand = getWords(n2);
+        if (thousand != null && thousand.length() != 0) {
+            res.append(thousand + "Thousand ");
+        }
+        res.append(getWords(n1));
+        return res.toString().trim();
+    }
+
+    private static String getWords(int num) {
+        if (num <= 0 || num > 999) return "";
+        String[] singles = new String[]{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        String[] teens = new String[]{"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        String[] tens = new String[]{"", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        StringBuilder res = new StringBuilder();
+        //百位数
+        if (100 <= num) {
+            res.append(singles[num / 100 - 1]);
+            res.append(" Hundred ");
+            num %= 100;
+        }
+
+        if (10 <= num && num <= 19) {//10~19
+            res.append(teens[num % 10]);
+            res.append(" ");
+        } else {
+            if (20 <= num) {//十位数
+                res.append(tens[num / 10 - 1]);
+                res.append(" ");
+                num %= 10;
+            }
+            if (0 < num) {//个位数
+                res.append(singles[num - 1]);
+                res.append(" ");
+            }
+        }
+
+        return res.toString();
     }
 
     /**
@@ -34,16 +111,16 @@ public class Integer_to_English_Words_273 {
      * 1.输入数字用千分符分割。穷举所有可能性，并建立映射表。
      * 2.从后向前计算的方案更优，无需提前计算高位数量级。
      * 3.任务拆解：数字映射，数量级计算，分割数字(千分符，每3个数字为一个unit)
-     *
+     * <p>
      * 算法：
      * 1.设places[]={"","Thousand","Million","Billion","Trillion"}，r为places的下标
-     *   设nun2Str={"1":"One","2":"Two",..,"10":"Ten","11":"Eleven",..,"20":"Twenty",..,"90";"Ninety","100":"Hundred"}
+     * 设nun2Str={"1":"One","2":"Two",..,"10":"Ten","11":"Eleven",..,"20":"Twenty",..,"90";"Ninety","100":"Hundred"}
      * 2.从后向前遍历。
      * 2.1.每三个数字(千分符)为一个unit
      * 2.2.IF unit>0，THEN res.append(places[r]),r++
      * 2.3.计算unit，并输出字符串
      * 3.反转res
-     *
+     * <p>
      * 计算3位数字的算法：
      * 1.边界判断，小于等于0或者大于等于1000，返回空串
      * 2.提取最低两位数字，设为tens
@@ -52,7 +129,7 @@ public class Integer_to_English_Words_273 {
      * 5.IF 20<tens<100,THEN 先输出个位,再输出十位
      * 6.提取高位(百位)数字,THEN 输出"Handred",输入码表对应的值
      * 7.反转
-     *
+     * <p>
      * 验证通过:
      * Runtime: 5 ms, faster than 82.46% of Java online submissions for Integer to English Words.
      * Memory Usage: 42.2 MB, less than 78.00% of Java online submissions for Integer to English Words.
@@ -214,9 +291,11 @@ public class Integer_to_English_Words_273 {
         do_func(1000000, "One Million");
         do_func(1000000000, "One Billion");
         do_func(1000000001, "One Billion One");
+        do_func(14, "Fourteen");
     }
 
     private static void do_func(int n, String expected) {
+        System.out.println(n);
         String ret = numberToWords(n);
         System.out.println(ret);
         System.out.println(expected.equals(ret));
