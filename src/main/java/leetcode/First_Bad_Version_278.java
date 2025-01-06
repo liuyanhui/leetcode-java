b347eda36adf080dd06313b5de78770a66a5ddd2
lyhLenovo <liuyanhui414@163.com>
2025-01-06 10:47:12 +0800

    round 3 , modify 278. You need to take a eye on how to calc 'mid'.

--- src/main/java/leetcode/First_Bad_Version_278.java
+++ src/main/java/leetcode/First_Bad_Version_278.java
@@ -5,11 +5,11 @@ package leetcode;
  * Easy
  * ------------------------------
  * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
- *
+ * <p>
  * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
- *
+ * <p>
  * You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
- *
+ * <p>
  * Example 1:
  * Input: n = 5, bad = 4
  * Output: 4
@@ -18,11 +18,11 @@ package leetcode;
  * call isBadVersion(5) -> true
  * call isBadVersion(4) -> true
  * Then 4 is the first bad version.
- *
+ * <p>
  * Example 2:
  * Input: n = 1, bad = 1
  * Output: 1
- *
+ * <p>
  * Constraints:
  * 1 <= bad <= n <= 2^31 - 1
  */
@@ -38,9 +38,24 @@ public class First_Bad_Version_278 {
         }
 
         public int firstBadVersion(int n) {
+            return firstBadVersion_1(n);
+        }
+
+        /**
+         * round 3
+         * Score[3] Lower is harder
+         * <p>
+         * firstBadVersion_1()中的mid的写法很关键。
+         *
+         * @param n
+         * @return
+         */
+
+        public int firstBadVersion_1(int n) {
             int l = 1, r = n;
             int mid = 0;
             while (l < r) {
+                //review 下面的代码不能写成 [mid = r - (r - l) / 2;]，因为无法处理l=4,r=5的情况。因为mid=5，会死循环。
                 mid = l / 2 + r / 2;
                 if (isBadVersion(mid)) {
                     r = mid;
@@ -61,7 +76,17 @@ public class First_Bad_Version_278 {
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution(909090);
-        System.out.println(solution.firstBadVersion(Integer.MAX_VALUE));
+        do_func(Integer.MAX_VALUE, 909090, 909090);
+        do_func(5, 4, 4);
+        do_func(5, 3, 3);
+    }
+
+
+    private static void do_func(int n, int bad, int expected) {
+        Solution solution = new Solution(bad);
+        int ret = solution.firstBadVersion(n);
+        System.out.println(ret);
+        System.out.println(ret == expected);
+        System.out.println("--------------");
     }
 }
