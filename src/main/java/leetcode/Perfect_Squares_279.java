9986dbebdb754500f42a1fc386499d10164ddb4c
lyhLenovo <liuyanhui414@163.com>
2025-01-10 11:43:18 +0800

    round 3 , modify 279.

--- src/main/java/leetcode/Perfect_Squares_279.java
+++ src/main/java/leetcode/Perfect_Squares_279.java
@@ -1,42 +1,103 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 279. Perfect Squares
  * Medium
  * --------------------------
  * Given an integer n, return the least number of perfect square numbers that sum to n.
- *
+ * <p>
  * A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
- *
+ * <p>
  * Example 1:
  * Input: n = 12
  * Output: 3
  * Explanation: 12 = 4 + 4 + 4.
- *
+ * <p>
  * Example 2:
  * Input: n = 13
  * Output: 2
  * Explanation: 13 = 4 + 9.
- *
+ * <p>
  * Constraints:
  * 1 <= n <= 10^4
  */
 public class Perfect_Squares_279 {
     public static int numSquares(int n) {
-        return numSquares_4(n);
+        return numSquares_r3_1(n);
+    }
+
+    /**
+     * round 3
+     * Score[2] Lower is harder
+     * <p>
+     * Thinking
+     * 1. navie solution
+     * 穷举+递归+缓存中间结果
+     * 公式为：
+     * F(1)=1
+     * F(2)=F(1)+F(2-1)
+     * F(3)=F(1)+F(3-1)=F(1)+F(2)
+     * F(4)=min(F(1)+F(3),F(2)+F(2))
+     * F(5)=min(F(1)+F(4),F(2)+F(3))
+     * F(n)=min(F(1)+F(n-1),F(2)+F(n-2),...,F(n-1)+F(1))
+     * =min(F(1)+F(n-1),F(2)+F(n-2),...,F(n/2)+F(n-n/2))
+     * 具体算法为：
+     * i = [1:n]:
+     * IF i is perfect number THEN
+     * F(i)=1
+     * ELSE
+     * F(i)=min(F(1)+F(i-1),F(2)+F(i-2),...,F(i/2)+F(i-i/2))
+     * Time Complexity: O(N*N)
+     * 2. 针对【1.】优化
+     * 因为 perfect number 是必选项，
+     * 所以计算
+     * F(i)=min(F(1)+F(i-1),F(2)+F(i-2),...,F(i/2)+F(i-i/2))
+     * 可以优化为
+     * F(i)=min(F(1)+F(i-1),F(4)+F(i-4),F(9)+F(i-9),F(16)+F(i-16),...)
+     * 即只遍历 perfect number ，忽略其他。
+     * Time Complexity: O(N*sqrt(N))
+     * <p>
+     * 验证通过：
+     * Runtime 37 ms Beats 50.32%
+     * Memory 42.80 MB Beats 67.08%
+     *
+     * @param n
+     * @return
+     */
+    public static int numSquares_r3_1(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        //初始化dp
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        //计算dp
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {//只从perfect squares中选.
+                if (j * j == i) {
+                    dp[i] = 1;
+                } else {
+                    dp[i] = Math.min(dp[i], dp[j * j] + dp[i - j * j]);
+                }
+            }
+        }
+        return dp[n];
     }
 
     /**
      * round 2
-     *
+     * <p>
      * 参考思路：
      * https://leetcode.com/problems/perfect-squares/discuss/71495/An-easy-understanding-DP-solution-in-Java
      * numSquares_2()
-     *
+     * <p>
      * DP思路：
      * dp[0]=0
      * dp[i]=min(dp[i],dp[i-j*j]+1) , 1<=j<=sqrt(n)
-     *
+     * <p>
      * 验证通过：
      * 执行用时：49 ms, 在所有 Java 提交中击败了18.10%的用户
      * 内存消耗：40.3 MB, 在所有 Java 提交中击败了92.08%的用户
@@ -66,9 +127,9 @@ public class Perfect_Squares_279 {
      * 思考：
      * 1.两部分内容：a.计算所有square number组合；b.找出the least number
      * 2.递归+缓存是一个思路
-     *
+     * <p>
      * review 递归算法可以转换为DP算法或使用栈的算法
-     *
+     * <p>
      * 验证通过：
      * Runtime: 780 ms, faster than 8.33% of Java online submissions for Perfect Squares.
      * Memory Usage: 41.3 MB, less than 96.72% of Java online submissions for Perfect Squares.
@@ -110,7 +171,7 @@ public class Perfect_Squares_279 {
      * https://leetcode.com/problems/perfect-squares/discuss/71495/An-easy-understanding-DP-solution-in-Java
      * 公式如下：
      * dp[n] = Min{ dp[n - i*i] + 1 },  n - i*i >=0 && i >= 1
-     *
+     * <p>
      * 验证通过：
      * Runtime: 53 ms, faster than 41.30% of Java online submissions for Perfect Squares.
      * Memory Usage: 39.9 MB, less than 22.51% of Java online submissions for Perfect Squares.
@@ -140,10 +201,11 @@ public class Perfect_Squares_279 {
      * DP思路，公式如下：
      * 1.dp[n]=min(dp[i]+dp[n-i-1]),0<i<n
      * 2.if Math.sqrt(i) * Math.sqrt(i) == i then dp[i]=1
-     *
+     * <p>
      * 验证通过：
      * Runtime: 1694 ms, faster than 5.01% of Java online submissions for Perfect Squares.
      * Memory Usage: 37.7 MB, less than 88.69% of Java online submissions for Perfect Squares.
+     *
      * @param n
      * @return
      */
@@ -174,15 +236,17 @@ public class Perfect_Squares_279 {
         do_func(13, 2);
         do_func(36, 1);
         do_func(81, 1);
-        do_func(80, 2);
+        do_func(20, 2);
         do_func(79, 4);
+        do_func(80, 2);
         do_func(1, 1);
         do_func(9999, 4);
     }
 
     private static void do_func(int nums, int expected) {
+        System.out.println("input=" + nums);
         int ret = numSquares(nums);
-        System.out.println(ret);
+        System.out.println("output=" + ret);
         System.out.println(ret == expected);
         System.out.println("--------------");
     }
