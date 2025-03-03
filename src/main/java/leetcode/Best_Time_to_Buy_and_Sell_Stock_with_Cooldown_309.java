1f82725300b174986e00857fbe52b2105037a876
lyhLenovo <liuyanhui414@163.com>
2025-03-03 09:47:12 +0800

    round 3 ,modify 309

--- src/main/java/leetcode/Best_Time_to_Buy_and_Sell_Stock_with_Cooldown_309.java
+++ src/main/java/leetcode/Best_Time_to_Buy_and_Sell_Stock_with_Cooldown_309.java
@@ -6,25 +6,54 @@ package leetcode;
  * ------------------
  * You are given an array prices where prices[i] is the price of a given stock on the ith day.
  * Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
- * After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+ * - After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
  * Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
- *
+ * <p>
  * Example 1:
  * Input: prices = [1,2,3,0,2]
  * Output: 3
  * Explanation: transactions = [buy, sell, cooldown, buy, sell]
- *
+ * <p>
  * Example 2:
  * Input: prices = [1]
  * Output: 0
- *
+ * <p>
  * Constraints:
  * 1 <= prices.length <= 5000
  * 0 <= prices[i] <= 1000
  */
 public class Best_Time_to_Buy_and_Sell_Stock_with_Cooldown_309 {
     public static int maxProfit(int[] prices) {
-        return maxProfit_3(prices);
+        return maxProfit_r3_1(prices);
+    }
+
+    /**
+     * round 3
+     * Score[2] Lower is harder
+     * <p>
+     * round3 : review 状态机中，动作（动词）是边，节点是名词（上一个动作完成后的结果）
+     * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/solutions/75928/share-my-dp-solution-by-state-machine-thinking/
+     *
+     * <p>
+     * 验证通过：
+     * Runtime 1 ms Beats 80.16%
+     * Memory 41.75 MB Beats 66.83%
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfit_r3_1(int[] prices) {
+        int[] s0 = new int[prices.length];
+        int[] s1 = new int[prices.length];
+        int[] s2 = new int[prices.length];
+        s1[0] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            s0[i] = Math.max(s0[i - 1], s2[i - 1]);
+            s1[i] = Math.max(s0[i - 1] - prices[i], s1[i - 1]);
+            s2[i] = s1[i - 1] + prices[i];
+        }
+
+        return Math.max(s0[prices.length - 1], Math.max(s1[prices.length - 1], s2[prices.length - 1]));
     }
 
     public static int maxProfit_3(int[] prices) {
@@ -68,9 +97,9 @@ public class Best_Time_to_Buy_and_Sell_Stock_with_Cooldown_309 {
     /**
      * 参考思路:
      * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75928/Share-my-DP-solution-(By-State-Machine-Thinking)
-     *
+     * <p>
      * 状态机的抽象是关键
-     *
+     * <p>
      * round2 : review 要画一画状态机的状态转换，注意：状态和动作要区分清楚，不能混在一起
      *
      * @param prices
